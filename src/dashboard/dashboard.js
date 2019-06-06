39588cf6f7d410e98d003b08a7f95c74f6c60cf7
Neil T <neiltz711@gmail.com>
2019-06-05 23:43:12 -0400

    Updated ChatList and Dashboard

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -1,10 +1,67 @@
 import React from 'react';
+import ChatListComponent from '../chatList/chatList';
+
+const firebase = require('firebase');
 
 class DashboardComponent extends React.Component {
     
-    render() {
-        return(<div>Hello World From Dashboard</div>)
+    constructor() {
+        super();
+        this.state = {
+            selectedChat: null,
+            newChatFormVisible: false,
+            email: null,
+            chats: []
+        };
     }
+
+    render() {
+        return(
+            <div>
+                <ChatListComponent history={this.props.history}
+                    newChatBtnFn={this.newChatBtnClicked}
+                    selectChatFn={this.selectChat}
+                    chats={this.state.chats}
+                    userEmail={this.state.email}
+                    selectedChatIndex={this.state.selectedChat}
+                />
+            </div>
+        );
+    }
+
+    selectChat = (chatIndex) => {
+        console.log('Selected a chat', chatIndex);
+    }
+
+    newChatBtnClicked = () => {
+        this.setState({
+            newChatFormVisible: true,
+            selectedChat: null
+        });
+    }
+
+    // Called automatically whenever the component is successfully mounted to the dom
+    componentDidMount = () => {
+        firebase.auth().onAuthStateChanged(async _usr => {
+            if(!_usr) {
+                this.props.history.push('/login');
+            } else { 
+                await firebase // Go into collection chats into user array returning user email
+                    .firestore()
+                    .collection('chats')
+                    .where('users', 'array-contains', _usr.email)
+                    .onSnapshot(async res => { // Realtime updates
+                        const chats = res.docs.map(_doc => _doc.data());
+                        await this.setState({ // setState is asynchronous
+                            email: _usr.email,
+                            chats: chats
+                        });
+                        console.log(this.state);
+                    })
+            }
+        })
+    }
+
 }
 
 export default DashboardComponent;
